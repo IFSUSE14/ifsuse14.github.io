d5b4b197d25364bd2bce6fc837a54a33114f6fb4
ifsuse14 <98948797+IFSUSE14@users.noreply.github.com>
2022-02-03 18:19:19 +0800

    NTEAWB

---
+++ script/script.js
@@ -0,0 +1,7 @@
+//按下按钮将文本"To Bing"改变成"To Goole"并改变图片"Bing Logo"为图片"Goole Logo".好像还没写完...
+function Bing()
+{
+    document.getElementById("Bing").innerHTML="az,失败了.作者还没写完呢hhh"
+}
+//这是控制台彩蛋awa
+console.log("????还有人开控制台？？那你一定想改什么吧awa如果不是的话.....那没事了")
